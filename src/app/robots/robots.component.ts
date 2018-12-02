78beafe8065334afe9f66a835a8da72d84dac248
Jashmae <jbaculpo@csumb.edu>
2018-12-02 14:37:12 -0800

    added files into the src/app/robots folder and src/app/robot-details

--- src/app/robots/robots.component.ts
+++ src/app/robots/robots.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NavbarService } from '../navbar.service';
 
 @Component({
   selector: 'app-robots',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RobotsComponent implements OnInit {
 
-  constructor() { }
+  robots$: Object;
+  cart = [];
+
+  constructor(
+    private rdataService: DataService,
+    private router: Router,
+    public nav: NavbarService) { }
 
   ngOnInit() {
+    this.nav.show();
+    if (localStorage.getItem('USER') == null) {
+      this.router.navigate(['/login']);
+    }
+    this.rdataService.getRobots().subscribe(
+      data => this.robots$ = data
+    );
+  }
+
+  addToCart(robotName) {
+    const robotCount = localStorage.getItem(robotName);
+    if (robotCount == null) {
+      localStorage.setItem(robotName, '1');
+    } else {
+      const num = parseInt(robotCount, 10);
+      const newn = num + 1;
+      localStorage.setItem(robotName, newn.toString());
+    }
+    this.router.navigate(['/checkout']);
   }
 
 }
