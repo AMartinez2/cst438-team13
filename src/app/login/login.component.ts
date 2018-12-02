f1205c68eb40f5dad3d4cd9c2ac1ddb3f475f391
Garred <gmurphy@csumb.edu>
2018-12-02 15:31:56 -0800

    checkout, login, and navbar

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { NavbarService } from './../navbar.service';
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  users$: Object;
+  constructor(
+    private dataService: DataService,
+    private router: Router,
+    private navbar: NavbarService) {
+    // Hide the navbar
+    navbar.hide();
+    // Get all users
+    this.dataService.getAllUsers().subscribe(
+      data => {
+        this.users$ = data;
+      }
+    );
+  }
 
   ngOnInit() {
+  }
+
+  loginAttempt(event) {
+    localStorage.clear();
+    // Retrieve the login form data from the DOM
+    event.preventDefault();
+    const target = event.target;
+    const DomUn = target.querySelector('#username').value;
+    const DomPw = target.querySelector('#password').value;
+    // Check to see if given user matches user in db
+    for (const usr in this.users$) {
+      if (DomUn === this.users$[usr]['usid']) {
+        this.router.navigate(['/robots']);
+        // Store user name in local storage
+        localStorage.setItem('USER', DomUn);
+      }
+    }
   }
 
 }
